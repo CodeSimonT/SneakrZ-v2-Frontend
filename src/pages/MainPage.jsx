ddaa86d145ad71d326ebb6179032f9bf41eb9c35
Simon_Turiano <simonturiano57@gmail.com>
2023-07-24 17:39:14 +0800

    fix the package.json

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Hero, Categories, JustIn, Featured, Slides } from "../mainPage";
+import { Hero, Categories, JustIn, Featured, Slides } from "../mainPage.jsx";
 import { fetchAllShoes } from "../redux/cart/getAllShoes";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
